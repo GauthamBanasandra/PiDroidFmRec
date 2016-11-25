933e6861bd25a3c1016f560b7fc18b7e52b3129f
Gautham BA <gautham.bangalore@gmail.com>
2016-11-25 23:59:56 +0530

    Making the app full screen.

--- Android/PiDroidFmRec/app/src/main/java/com/scheduler/record/radio/pidroidfmrec/MainActivity.java
+++ Android/PiDroidFmRec/app/src/main/java/com/scheduler/record/radio/pidroidfmrec/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TimePicker;
@@ -27,8 +28,10 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // Remove title bar.
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // Remove notification bar.
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         final TimePicker timePickerWakeUp = (TimePicker) findViewById(R.id.timePicker_wake_up);
