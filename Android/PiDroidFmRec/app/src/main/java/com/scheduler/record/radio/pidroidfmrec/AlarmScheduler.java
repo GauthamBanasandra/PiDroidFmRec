3708720c220d8d92dd8f4269bfc1eae56fede8ab
Gautham BA <gautham.bangalore@gmail.com>
2016-11-25 23:20:28 +0530

    Designed the UI to select the time for waking up the phone.

---
+++ Android/PiDroidFmRec/app/src/main/java/com/scheduler/record/radio/pidroidfmrec/AlarmScheduler.java
@@ -0,0 +1,9 @@
+package com.scheduler.record.radio.pidroidfmrec;
+
+/**
+ * Created by gauth_000 on 25-Nov-16.
+ */
+public class AlarmScheduler
+{
+
+}
--- Android/PiDroidFmRec/app/src/main/java/com/scheduler/record/radio/pidroidfmrec/MainActivity.java
+++ Android/PiDroidFmRec/app/src/main/java/com/scheduler/record/radio/pidroidfmrec/MainActivity.java
@@ -1,7 +1,11 @@
 package com.scheduler.record.radio.pidroidfmrec;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +15,21 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TimePicker timePickerWakeUp = (TimePicker) findViewById(R.id.timePicker_wake_up);
+        Button buttonSet = (Button) findViewById(R.id.button_set);
+
+        assert buttonSet != null;
+        buttonSet.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                assert timePickerWakeUp != null;
+                Toast.makeText(MainActivity.this, timePickerWakeUp.getHour() + "\t" +
+                        timePickerWakeUp.getMinute(), Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }
