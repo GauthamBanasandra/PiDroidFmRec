54831830846aa5208c3a7f45bdd4dce54ad9639d
GauthamBanasandra <gautham.bangalore@gmail.com>
2016-11-28 22:52:29 +0530

    Documented deviceController.

--- Go/deviceController/cmdProcessor.go
+++ Go/deviceController/cmdProcessor.go
@@ -1,1 +1,11 @@
 package deviceController
+
+const (
+	prg = "adb"
+	cmdPrefix = "shell input "
+	cmdWakeUp = cmdPrefix + "keyevent 26"
+	cmdUnlock = cmdPrefix + "keyevent 82"
+	cmdEnterKey = cmdPrefix + "keyevent 66"
+	cmdEnterPwd = cmdPrefix + "text "
+	cmdConn = "connect "
+)
--- Go/deviceController/deviceController.go
+++ Go/deviceController/deviceController.go
@@ -8,36 +8,40 @@ import (
 	"strings"
 )
 
-const (
-	prg = "adb"
-	cmdPrefix = "shell input "
-	cmdWakeUp = cmdPrefix + "keyevent 26"
-	cmdUnlock = cmdPrefix + "keyevent 82"
-	cmdEnterKey = cmdPrefix + "keyevent 66"
-	cmdEnterPwd = cmdPrefix + "text "
-	cmdConn = "connect "
-)
-
+/*
+Struct for representing a list of commands.
+A channel is included for communicating intermediate outputs.
+*/
 type CommandList struct {
 	Commands   []string
 	CmdChannel chan string
 }
 
+// Unlocks the Android device given the IP address and password.
 func UnlockDevice(ip string, password string) {
+	// Creating a channel to display outputs of each command immediately after executing.
 	cmdChannel := make(chan string)
 	commands := CommandList{[]string{cmdConn + ip, cmdWakeUp, cmdUnlock, cmdEnterPwd + password, cmdEnterKey},
 		cmdChannel}
 
+	// Begin executing commands.
 	go executeCmdShell(commands)
 	for c := range cmdChannel {
+		// Print the intermediate outputs.
 		fmt.Println(c)
 	}
 }
 
+/*
+Executes a given command or a list of commands.
+Logic is written to infer the type.
+ */
 func executeCmdShell(cmd interface{}) string {
 	var output string
 
+	// Get the type of the parameter passed.
 	switch cmd.(type) {
+	// If it's just one command.
 	case string:
 		output, err := exec.Command(prg, strings.Split(cmd.(string), " ")...).CombinedOutput()
 		if err != nil {
@@ -45,13 +49,16 @@ func executeCmdShell(cmd interface{}) string {
 		}
 		return string(output)
 
+	// If it's a list of commands.
 	case CommandList:
+		// Get the channel.
 		channel := cmd.(CommandList).CmdChannel
 		for _, c := range cmd.(CommandList).Commands {
 			output, err := exec.Command(prg, strings.Split(c, " ")...).CombinedOutput()
 			if err != nil {
 				os.Stderr.WriteString("error executing command " + err.Error())
 			}
+			// Write each output to the channel.
 			channel <- string(output)
 		}
 		close(channel)
