d2124aa809d61af4eda164fbdaa33e036678153b
GauthamBanasandra <gautham.bangalore@gmail.com>
2016-12-03 21:35:43 +0530

    Documented recordScheduler.

--- Go/scheduler/recordScheduler.go
+++ Go/scheduler/recordScheduler.go
@@ -8,15 +8,21 @@ import (
 	"fmt"
 )
 
+// Structure representing the action to be scheduled.
 type ScheduleAction struct {
 	Time   jsonReader.Time
 	Action func() ()
 }
 
+// Converts time in hh-mm-ss format to seconds.
 func GetSeconds(time jsonReader.Time) int {
 	return time.Hh * 60 * 60 + time.Mm * 60 + time.Ss
 }
 
+/*
+Returns the difference between the given 2 times(A, B).
+If A-B < 0, then it adds 24*60*60 (total seconds in a day) and returns.
+*/
 func GetDiffSeconds(fromTime, toTime jsonReader.Time) int {
 	fromSec := GetSeconds(fromTime)
 	toSec := GetSeconds(toTime)
@@ -29,6 +35,7 @@ func GetDiffSeconds(fromTime, toTime jsonReader.Time) int {
 	}
 }
 
+// Returns the current time in hh-mm-ss format.
 func GetTimeNow() (hh, mm, ss int) {
 	timeSplice := strings.Split(time.Now().Format("15:04:05"), ":")
 	hh, _ = strconv.Atoi(timeSplice[0])
@@ -38,6 +45,10 @@ func GetTimeNow() (hh, mm, ss int) {
 	return
 }
 
+/*
+Schedules an alarm with the corresponding action to be executed.
+Also, it returns a channel to signal its triggering.
+*/
 func ScheduleAlarm(scheduler ScheduleAction) (endSignal chan string) {
 	endSignal = make(chan string)
 	go func() {
@@ -57,6 +68,7 @@ func ScheduleAlarm(scheduler ScheduleAction) (endSignal chan string) {
 	return
 }
 
+// Schedules start and stop of recording.
 func ScheduleRecording(start, stop ScheduleAction) (endStartSignal, endStopSignal chan string) {
 	endStartSignal = ScheduleAlarm(start)
 	endStopSignal = ScheduleAlarm(stop)
